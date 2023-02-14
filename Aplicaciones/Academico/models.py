4b1fe299340e36b4a102abf158b699f2a4126d3c
KevinSucapuca <ksucapuca007@gmail.com>
2023-02-14 14:17:34 -0500

    Add migrations

--- Aplicaciones/Academico/models.py
+++ Aplicaciones/Academico/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Cursor(models.Model):
+class Curso(models.Model):
     codigo = models.CharField(primary_key=True,  max_length=6)
     nombre = models.CharField(max_length=100)
     creditos = models.PositiveBigIntegerField()
--- CursoAcademico/settings.py
+++ CursoAcademico/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Aplicaciones.Academico',
+    'Aplicaciones.Academico.apps',
+
+    
 ]
 
 MIDDLEWARE = [
@@ -77,7 +80,7 @@ WSGI_APPLICATION = 'CursoAcademico.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'CursoAcademico.db',
     }
 }
 
