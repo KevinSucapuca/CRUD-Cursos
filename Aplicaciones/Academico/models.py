c82c14542929f3eb356b345dca2be581aa3353ba
KevinSucapuca <ksucapuca007@gmail.com>
2023-02-14 13:25:38 -0500

    Add modelo academico

--- Aplicaciones/Academico/models.py
+++ Aplicaciones/Academico/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Cursor(models.Model):
+    codigo = models.CharField(primary_key=True,  max_length=6)
+    nombre = models.CharField(max_length=100)
+    creditos = models.PositiveBigIntegerField()
+    
+
+    
