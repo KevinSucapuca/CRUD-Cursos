5f5360d32a7ea5197fd26e7d143e49cc2324af37
KevinSucapuca <ksucapuca007@gmail.com>
2023-02-14 20:52:13 -0500

    add ruta editar curso

--- Aplicaciones/Academico/urls.py
+++ Aplicaciones/Academico/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
 path('',views.home),
 path('registrarCurso/',views.registrarCurso),
+path('editarCurso/<codigo>',views.editarCurso),
 path('eliminarCurso/<codigo>',views.eliminarCurso),
 
 
