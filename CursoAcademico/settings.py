f1e2b1552afa541ebc75062b41e932912ba456c3
KevinSucapuca <ksucapuca007@gmail.com>
2023-02-15 16:21:05 -0500

    update settings

--- CursoAcademico/settings.py
+++ CursoAcademico/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -134,4 +134,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
