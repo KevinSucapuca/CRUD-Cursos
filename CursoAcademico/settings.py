3633c4e8b7b50a02c11ab98b6c2c659e3bd60648
KevinSucapuca <ksucapuca007@gmail.com>
2023-02-15 17:11:41 -0500

    update settings

--- CursoAcademico/settings.py
+++ CursoAcademico/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
@@ -132,6 +133,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
