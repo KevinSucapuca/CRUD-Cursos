a209608465de61db391d07bd336d800112cec118
KevinSucapuca <ksucapuca007@gmail.com>
2023-02-14 20:25:42 -0500

    add función crear curso

--- Aplicaciones/Academico/views.py
+++ Aplicaciones/Academico/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Curso
 # Create your views here.
 
@@ -6,3 +6,13 @@ def home(request):
     cursosListados = Curso.objects.all()
     
     return render(request, 'GestionCursos.html', {'cursos':cursosListados})
+
+
+def registrarCurso(request):
+    codigo=request.POST['txtcodigo']
+    nombre=request.POST['txtnombre']
+    creditos=request.POST['numcreditos']
+    
+    curso = Curso.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
+    return redirect('/')
+
