6ddfd12ec5b59f73f8f95888f25ecd1182409ba9
KevinSucapuca <ksucapuca007@gmail.com>
2023-08-06 23:16:26 -0500

    add view for error 404

--- Aplicaciones/Academico/views.py
+++ Aplicaciones/Academico/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Curso
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
@@ -15,12 +16,23 @@ def contacto(request):
 
 
 def registrarCurso(request):
-    codigo=request.POST['txtcodigo']
-    nombre=request.POST['txtnombre']
-    creditos=request.POST['numcreditos']
-    
-    curso = Curso.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
-    return redirect('/')
+    curso_existe = False
+    if request.method == 'POST':
+        codigo=request.POST['txtcodigo']
+        nombre=request.POST['txtnombre']
+        creditos=request.POST['numcreditos']
+        
+        curso_existe = Curso.objects.filter(codigo__iexact=codigo).exists()
+        if curso_existe:
+            messages.error(request, "El código del Curso ya existe")
+            return redirect('/')
+        else:
+            Curso.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
+            messages.success(request, "Curso Registrado Correctamente")
+            return redirect('/')
+    return render(request, 'GestionCursos.html', {'curso_existe': curso_existe})
+
+
 
 def edicionCurso(request,codigo):
     curso = Curso.objects.get(codigo=codigo)
@@ -35,11 +47,16 @@ def editarCurso(request):
     curso.nombre = nombre
     curso.creditos = creditos
     curso.save()
+    messages.success(request, "Curso editado correctamente")
     return redirect('/')
     
 
 def eliminarCurso(request,codigo):
     curso = Curso.objects.get(codigo=codigo)
     curso.delete()
+    messages.success(request, "Curso eliminado correctamente")
     return redirect('/')
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
     
